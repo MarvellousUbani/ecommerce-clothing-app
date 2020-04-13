e539459d64a12a45a08edf0655d7ecf7a9c240da
dflexcee <marvellousubani@gmail.com>
2020-04-14 00:39:12 +0100

    finishing signin method

--- src/components/sign-in/signin.component.jsx
+++ src/components/sign-in/signin.component.jsx
@@ -4,7 +4,7 @@ import './signin.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component'
 
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils'
 
 class Signin extends React.Component{
     constructor(props){
@@ -17,10 +17,17 @@ class Signin extends React.Component{
 
    }
 
-    handleSubmit = event => {
+    handleSubmit = asyn event => {
         event.preventDefault();
+        const {email, password} = this.state;
 
-        this.setState({email:'', password:''})
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email:'', password:''});
+        } catch(error){
+            console.log(error);
+        }
+       
     }
 
     handleChange = event =>{
