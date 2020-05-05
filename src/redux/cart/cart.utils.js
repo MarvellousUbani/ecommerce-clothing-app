14aca22ca05c1b8713f321bbe6df3f406183e14f
dflexcee <marvellousubani@gmail.com>
2020-05-05 12:26:14 +0100

    adding in memoized selectors for parts and the cart item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,11 +1,11 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existingCartItem = cardItems.find(
-        cartItem => cartItem.id == cartItemToAdd.id
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToAdd.id
     );
 
-    if(exisitingCartItem){
+    if(existingCartItem){
         return cartItems.map(cartItem => 
-            cartItem.id == cartItemToAdd.id ?
+            cartItem.id === cartItemToAdd.id ?
             {...cartItem, quantity: cartItem.quantity + 1}
             : cartItem
             )
