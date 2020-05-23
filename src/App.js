c1b18e458d8354aa6e8d05fbb0b1b914049dff92
Marvellous Ubani <marvellousubani@gmail.com>
2020-05-23 23:11:16 +0100

    fixed routed issue

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route exact path='/signin' render={() => this.props.currentUser? <Redirect to='/' /> : SignInAndSignUpPage} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/'/>) : (< SignInAndSignUpPage />) } />
           <Route exact path='/checkout' component={CheckoutPage} />
         </Switch>
       </div>
